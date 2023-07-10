14a5de44220c4d1cfe32c57e1d635315c240bd66
limeogus <!dlaeogus1>
2023-07-10 21:44:25 +0900

    Test @After 적용

--- JavaStudy/src/CalculatorTest.java
+++ JavaStudy/src/CalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Before;
 
 public class CalculatorTest {
@@ -10,15 +11,23 @@ public class CalculatorTest {
     @Before
     public void setup(){
         cal = new Calculator();
+        System.out.println("before");
     }
 
     @Test
     public void add(){
         assertEquals(9, cal.add(6, 3));
+        System.out.println("add");
     }
 
     @Test
     public void substract(){
         assertEquals(3, cal.substract(6, 3));
+        System.out.println("sub");
+    }
+
+    @After
+    public void teardown(){
+        System.out.println("teardown");
     }
 }
