3e97349fd558592f8ddede19a191344e9b7eeb58
limeogus <!dlaeogus1>
2023-07-10 21:15:36 +0900

    main 복잡성 줄이기

--- JavaStudy/src/CalculatorMain.java
+++ JavaStudy/src/CalculatorMain.java
@@ -1,9 +1,25 @@
 public class CalculatorMain {
     public static void main(String[] args) throws Exception {
         Calculator cal = new Calculator();
-        System.out.println(cal.add(9, 3));
-        System.out.println(cal.substract(9, 3));
-        System.out.println(cal.multiply(9, 3));
+        divide(cal);
+        multiply(cal);
+        substract(cal);
+        add(cal);
+    }
+
+    private static void divide(Calculator cal){
         System.out.println(cal.divide(9, 3));
+    }
+
+    private static void multiply(Calculator cal){
+        System.out.println(cal.multiply(9, 3));
+    }
+
+    private static void substract(Calculator cal){
+        System.out.println(cal.substract(9, 3));
+    }
+
+    private static void add(Calculator cal){
+        System.out.println(cal.add(9, 3));
     }
 }
