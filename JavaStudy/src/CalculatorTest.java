b0863179851165d1afb66298ab43eeb9ebb29d2d
limeogus <!dlaeogus1>
2023-07-10 21:32:40 +0900

    Test 자동화

--- JavaStudy/src/CalculatorTest.java
+++ JavaStudy/src/CalculatorTest.java
@@ -1,15 +1,16 @@
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     @Test
     public void add(){
         Calculator cal = new Calculator();
-        System.out.println(cal.add(6,3));
+        assertEquals(9, cal.add(6, 3));
     }
 
     @Test
     public void substract(){
         Calculator cal = new Calculator();
-        System.out.println(cal.substract(6, 3));
+        assertEquals(3, cal.substract(6, 3));
     }
 }
